1c16ffa3c4be63d4b6539c19e9aa514855e30f14
Garvitraj Pandey <pandeygarvitraj@outlook.com>
2021-08-16 12:04:32 +0530

    Update caloriemeter.m

--- Code/caloriemeter.m
+++ Code/caloriemeter.m
@@ -112,4 +112,223 @@ classdef caloriemeter_matlab < matlab.apps.AppBase
         IdliCheckBox                   matlab.ui.control.CheckBox
         CaloriemeterLabel              matlab.ui.control.Label
     end
-    
+     % Callbacks that handle component events
+    methods (Access = private)
+        % Close request function: UIFigure
+        function ButtonPushed(app, event)
+           
+        end
+        % Callback function
+        function BreakFastDropDownValueChanged(app, event)
+                       
+        end
+        % Callback function
+        function FatsEditFieldValueChanged(app, event)
+      
+        end
+        % Callback function
+        function BreakFastDropDownOpening(app, event)
+        end
+        % Callback function
+        function DropDownValueChanged(app, event)
+            
+        end
+        % Value changed function: IdliCheckBox
+        function IdliCheckBoxValueChanged(app, event)
+            value = app.IdliCheckBox.Value;
+           
+            switch value
+                case 0
+                    app.MultiplierEditField.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField.Visible = 'on';
+            end
+        end
+        % Value changed function: SambharCheckBox
+        function SambharCheckBoxValueChanged(app, event)
+            value = app.SambharCheckBox.Value;
+                        
+            switch value
+                case 0
+                    app.MultiplierEditField_2.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_2.Visible = 'on';
+            end
+        end
+        % Button pushed function: CalculateForBreakfastButton
+        function CalculateForBreakfastButtonPushed(app, event)
+            %calculation
+            
+            result = (app.MultiplierEditField.Value*58)+(app.MultiplierEditField_2.Value*152)+(app.MultiplierEditField_3.Value*168)+(app.MultiplierEditField_4.Value*135)+(app.MultiplierEditField_5.Value*70)+(app.MultiplierEditField_6.Value*180)+(app.MultiplierEditField_7.Value*200)+(app.MultiplierEditField_8.Value*219)+(app.MultiplierEditField_9.Value*95)+(app.MultiplierEditField_10.Value*49);
+            
+            app.TotalCaloriesEditField.Value = result;
+        end
+        % Value changed function: VadaCheckBox
+        function VadaCheckBoxValueChanged(app, event)
+            value = app.VadaCheckBox.Value;
+            
+             switch value
+                case 0
+                    app.MultiplierEditField_3.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_3.Visible = 'on';
+            end
+        end
+        % Value changed function: UpmaCheckBox
+        function UpmaCheckBoxValueChanged(app, event)
+            value = app.UpmaCheckBox.Value;
+            
+            switch value
+                case 0
+                    app.MultiplierEditField_8.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_8.Visible = 'on';
+            end
+        end
+        % Value changed function: PohaCheckBox_2
+        function PohaCheckBox_2ValueChanged(app, event)
+            value = app.PohaCheckBox_2.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_7.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_7.Visible = 'on';
+            end
+        end
+        % Value changed function: UttapamCheckBox
+        function UttapamCheckBoxValueChanged(app, event)
+            value = app.UttapamCheckBox.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_6.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_6.Visible = 'on';
+            end
+        end
+        % Value changed function: PomegranateCheckBox
+        function PomegranateCheckBoxValueChanged(app, event)
+            value = app.PomegranateCheckBox.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_5.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_5.Visible = 'on';
+            end
+        end
+        % Value changed function: AppleCheckBox
+        function AppleCheckBoxValueChanged(app, event)
+            value = app.AppleCheckBox.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_10.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_10.Visible = 'on';
+            end
+        end
+        % Value changed function: BananaCheckBox
+        function BananaCheckBoxValueChanged(app, event)
+            value = app.BananaCheckBox.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_9.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_9.Visible = 'on';
+            end
+        end
+        % Value changed function: DosaCheckBox
+        function DosaCheckBoxValueChanged(app, event)
+            value = app.DosaCheckBox.Value;
+            
+            switch value
+                case 0
+                    app.MultiplierEditField_4.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_4.Visible = 'on';
+            end
+        end
+        % Value changed function: MultiplierEditField_2
+        function MultiplierEditField_2ValueChanged(app, event)
+        
+            
+        end
+        % Button pushed function: CalculateForLunchButton
+        function CalculateForLunchButtonPushed(app, event)
+            result2 = (app.MultiplierEditField_11.Value*258)+(app.MultiplierEditField_12.Value*320)+(app.MultiplierEditField_13.Value*300)+(app.MultiplierEditField_14.Value*120)+(app.MultiplierEditField_15.Value*262)+(app.MultiplierEditField_16.Value*178)+(app.MultiplierEditField_17.Value*223)+(app.MultiplierEditField_18.Value*178)+(app.MultiplierEditField_19.Value*295)+(app.MultiplierEditField_20.Value*201);
+            
+            app.TotalCaloriesEditField_2.Value = result2;
+        end
+        % Button pushed function: CalculateForSnacksButton
+        function CalculateForSnacksButtonPushed(app, event)
+            result3 = (app.MultiplierEditField_21.Value*336)+(app.MultiplierEditField_22.Value*367)+(app.MultiplierEditField_23.Value*92)+(app.MultiplierEditField_24.Value*220)+(app.MultiplierEditField_25.Value*3)+(app.MultiplierEditField_26.Value*260)+(app.MultiplierEditField_27.Value*560)+(app.MultiplierEditField_28.Value*140)+(app.MultiplierEditField_29.Value*200)+(app.MultiplierEditField_30.Value*300);
+            
+            app.TotalCaloriesEditField_3.Value = result3;
+        end
+        % Button pushed function: CalculateForDinnerButton
+        function CalculateForDinnerButtonPushed(app, event)
+            result4 = (app.MultiplierEditField_31.Value*266)+(app.MultiplierEditField_22.Value*226)+(app.MultiplierEditField_33.Value*163)+(app.MultiplierEditField_34.Value*69)+(app.MultiplierEditField_35.Value*120)+(app.MultiplierEditField_36.Value*262)+(app.MultiplierEditField_37.Value*178)+(app.MultiplierEditField_38.Value*150)+(app.MultiplierEditField_39.Value*158)+(app.MultiplierEditField_40.Value*170);
+            
+            app.TotalCaloriesEditField_4.Value = result4;
+        end
+        % Value changed function: ParathaCheckBox
+        function ParathaCheckBoxValueChanged(app, event)
+            value = app.ParathaCheckBox.Value;
+                       
+            switch value
+                case 0
+                    app.MultiplierEditField_11.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_11.Visible = 'on';
+            end
+        end
+        % Value changed function: PaneerParathaCheckBox
+        function PaneerParathaCheckBoxValueChanged(app, event)
+            value = app.PaneerParathaCheckBox.Value;
+                    
+            switch value
+                case 0
+                    app.MultiplierEditField_12.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_12.Visible = 'on';
+            end
+        end
+        % Value changed function: AlooParathaCheckBox
+        function AlooParathaCheckBoxValueChanged(app, event)
+            value = app.AlooParathaCheckBox.Value;
+           
+            switch value
+                case 0
+                    app.MultiplierEditField_14.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_14.Visible = 'on';
+            end
+        end
+        % Value changed function: ChapatiCheckBox
+        function ChapatiCheckBoxValueChanged(app, event)
+            value = app.ChapatiCheckBox.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_13.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_13.Visible = 'on';
+            end
+        end
+        % Value changed function: TandooriNaanCheckBox
+        function TandooriNaanCheckBoxValueChanged(app, event)
+            value = app.TandooriNaanCheckBox.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_18.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_18.Visible = 'on';
+            end
+        end
+        % Value changed function: DaalCheckBox
+        function DaalCheckBoxValueChanged(app, event)
+            value = app.DaalCheckBox.Value;
+            switch value
+                case 0
+                    app.MultiplierEditField_17.Visible = 'off';
+                case 1 
+                    app.MultiplierEditField_17.Visible = 'on';
+            end
+        end
